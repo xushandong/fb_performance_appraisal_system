d38a1fb9d0ffb1d43f5a90bc80889e8c5ada3329
善栋 <xusd@fshows.com>
2019-11-14 17:53:59 +0800

    初始化项目

---
+++ .tea/entryFiles-development/index$.web.js
@@ -0,0 +1,5 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../client/pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../client/pages/mine/mine?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
---
+++ .tea/entryFiles-development/index$.worker.js
@@ -0,0 +1,29 @@
+if(!self.__appxInited) {
+self.__appxInited = 1;
+
+
+require('./config$');
+
+
+var AFAppX = self.AFAppX.getAppContext
+  ? self.AFAppX.getAppContext().AFAppX
+  : self.AFAppX;
+self.getCurrentPages = AFAppX.getCurrentPages;
+self.getApp = AFAppX.getApp;
+self.Page = AFAppX.Page;
+self.App = AFAppX.App;
+self.my = AFAppX.bridge || AFAppX.abridge;
+self.abridge = self.my;
+self.Component = AFAppX.WorkerComponent || function(){};
+self.$global = AFAppX.$global;
+self.requirePlugin = AFAppX.requirePlugin;
+        
+
+
+function success() {
+require('../../client/app');
+require('../../client/pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../client/pages/mine/mine?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
+}
+self.bootstrapApp ? self.bootstrapApp({ success }) : success();
+}
